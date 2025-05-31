3c1c6116e16a7e67fac265df04b2884e7f543f05
horexdev <nikita.horex@icloud.com>
2025-06-01 04:18:55 +0500

    Added file extensions in list

--- src/app/entities/cloudFile/ui/file-item/FileItem.tsx
+++ src/app/entities/cloudFile/ui/file-item/FileItem.tsx
@@ -13,7 +13,7 @@ const FileItem = ({ data }: { data: CloudFile }) => {
         <div className={`${styles.file} flex gap-2 items-center justify-between p-3 bg-[#212020] rounded-xl border-2 border-[#1565C0] hover:border-inherit duration-[0.3s]`}>
             <div className="grid grid-cols-[5%_30%_20%_1fr] w-[80%] gap-10">
                 <FilePreview extension={data.extension} />
-                <span className="my-auto text-ellipsis text-clip overflow-hidden">{data.name}</span>
+                <span className="my-auto text-ellipsis text-clip overflow-hidden">{data.name}{data.extension ? `.${data.extension}` : ''}</span>
                 <span className="my-auto text-ellipsis">{formatDate(data.modifiedAt)}</span>
                 <span className="my-auto text-ellipsis">{formatBytes(data.size)}</span>
             </div>
