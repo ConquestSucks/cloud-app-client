0e2495a27d1fecebb1b9ef1431077e81ff498192
ConquestSucks <91684467+ConquestSucks@users.noreply.github.com>
2025-05-31 18:45:21 +0500

    dummy storageQuota page

--- app/(routes)/dashboard/quota/page.tsx
+++ app/(routes)/dashboard/quota/page.tsx
@@ -1,9 +1,14 @@
+import DisplayStorageQuota from '@/app/features/storage-quota/ui/displayStorageQuota'
+import PageTitle from '@/app/shared/ui/pageTitle'
 import React from 'react'
 
 const Quota = () => {
-  return (
-    <div>Quota</div>
-  )
+    return (
+        <div className='dashboard-page'>
+            <PageTitle title="Хранилище" />
+            <DisplayStorageQuota />
+        </div>
+    )
 }
 
 export default Quota
---
+++ src/app/features/storage-quota/ui/displayStorageQuota.tsx
@@ -0,0 +1,34 @@
+export default function CategoryQuotaBar() {
+  const total = 100;
+  const video = 40;
+  const photo = 30;
+  const other = 10;
+  const used = video + photo + other;
+
+  return (
+    <div className="grow">
+      <p className="text-sm mb-2 text-gray-300 ">Использовано: <span className="text-3xl text-white">{used} ГБ</span> из {total} ГБ</p>
+
+      <div className="h-4 rounded-full overflow-hidden bg-gray-300 flex">
+        <div
+          className="bg-blue-500"
+          style={{ width: `${(video / total) * 100}%` }}
+        />
+        <div
+          className="bg-green-500"
+          style={{ width: `${(photo / total) * 100}%` }}
+        />
+        <div
+          className="bg-orange-400"
+          style={{ width: `${(other / total) * 100}%` }}
+        />
+      </div>
+
+      <div className="flex gap-4 mt-2 text-xs text-gray-300">
+        <span>Видео: {video} ГБ</span>
+        <span>Фото: {photo} ГБ</span>
+        <span>Другое: {other} ГБ</span>
+      </div>
+    </div>
+  );
+}
